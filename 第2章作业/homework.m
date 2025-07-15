14418f18cd359de1fd4a5a0d43415ab8ded50b7f
George <936728057@qq.com>
2025-07-15 16:32:39 +0800

    第1章~第3章的脚本学习

---
+++ 第2章作业/homework.m
@@ -0,0 +1,14 @@
+sin(3/2*pi)     % ans=sin(3/2*pi)=-1
+floor(1314/52)
+mod(1314,52)
+61452-mod(61452,1000)
+x=001
+c=mod(x,10)
+x=floor(x/10)
+b=mod(x,10)
+x=floor(x/10)
+a=x
+
+x=1.2
+a=fix(x)
+mod(x,1)
